177b7c1507702a801b0712ea39aaf8428d688313
EDIT.OG_ <rsak0730@gmail.com>
2026-08-31 13:42:10 +0530

    update server.js

--- server.js
+++ server.js
@@ -7,13 +7,12 @@ const server = http.createServer((req, res) => {
 });
 
 const wss = new WebSocket.Server({ server });
-const rooms = {};
+const rooms = {}; // Format: { roomCode: { host: ws, peers: [], currentTrack: null, isPlaying: false, currentTime: 0 } }
 
 wss.on('connection', (ws) => {
     let currentRoom = null;
     ws.isAlive = true;
 
-    // Respond to client pings to keep Render connection alive
     ws.on('pong', () => {
         ws.isAlive = true;
     });
@@ -33,7 +32,7 @@ wss.on('connection', (ws) => {
                 ws.clientAccountId = data.accountId || 'SYNC-0000';
 
                 if (!rooms[currentRoom]) {
-                    rooms[currentRoom] = { host: ws, peers: [] };
+                    rooms[currentRoom] = { host: ws, peers: [], currentTrack: null, isPlaying: false, currentTime: 0 };
                 } else {
                     rooms[currentRoom].host = ws;
                 }
@@ -58,8 +57,47 @@ wss.on('connection', (ws) => {
                 updateRoomPeers(currentRoom);
                 broadcastToRoom(currentRoom, { type: 'NOTIFICATION', message: `${ws.clientName} joined the room` }, ws);
 
-            } else if (['CONTROL', 'PLAY_TRACK', 'VOLUME', 'REACTION'].includes(data.type)) {
+                // Instantly sync currently playing track to the newly joined member
+                const room = rooms[currentRoom];
+                if (room.currentTrack) {
+                    ws.send(JSON.stringify({
+                        type: 'PLAY_TRACK',
+                        title: room.currentTrack,
+                        currentTime: room.currentTime || 0
+                    }));
+                    if (room.isPlaying) {
+                        ws.send(JSON.stringify({
+                            type: 'CONTROL',
+                            action: 'play',
+                            time: room.currentTime || 0
+                        }));
+                    }
+                }
+
+            } else if (data.type === 'PLAY_TRACK') {
+                if (currentRoom && rooms[currentRoom]) {
+                    rooms[currentRoom].currentTrack = data.title;
+                    rooms[currentRoom].isPlaying = true;
+                    rooms[currentRoom].currentTime = data.currentTime || 0;
+                }
                 broadcastToRoom(currentRoom, data, ws);
+
+            } else if (data.type === 'CONTROL') {
+                if (currentRoom && rooms[currentRoom]) {
+                    if (data.action === 'play' || data.action === 'seek') {
+                        rooms[currentRoom].isPlaying = true;
+                    } else if (data.action === 'pause') {
+                        rooms[currentRoom].isPlaying = false;
+                    }
+                    if (typeof data.time === 'number') {
+                        rooms[currentRoom].currentTime = data.time;
+                    }
+                }
+                broadcastToRoom(currentRoom, data, ws);
+
+            } else if (['VOLUME', 'REACTION'].includes(data.type)) {
+                broadcastToRoom(currentRoom, data, ws);
+
             } else if (data.type === 'ROOM_CLOSED') {
                 broadcastToRoom(currentRoom, { type: 'ROOM_CLOSED' });
                 broadcastToRoom(currentRoom, { type: 'NOTIFICATION', message: `${ws.clientName || 'Host'} closed the room` });
@@ -90,7 +128,6 @@ wss.on('connection', (ws) => {
     });
 });
 
-// Periodic background check to drop dead connections cleanly
 const interval = setInterval(() => {
     wss.clients.forEach((ws) => {
         if (ws.isAlive === false) return ws.terminate();
