6c46f1fba09eb05b1068c4994e8a6fc06b47b9ef
rsak0730-cmyk <321288827+rsak0730-cmyk@users.noreply.github.com>
2026-09-04 00:23:04 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'syncbeat-v5';
+const CACHE_NAME = 'syncbeat-v6';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
