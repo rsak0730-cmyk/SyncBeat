9532921f30911b09f998dd00645640669e03cdcd
rsak0730-cmyk <321288827+rsak0730-cmyk@users.noreply.github.com>
2026-09-04 00:19:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,28 @@
-const CACHE_NAME = 'syncbeat-v4';
+const CACHE_NAME = 'syncbeat-v5';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  'https://raw.githubusercontent.com/pwa-builder/pwa-starter/main/public/assets/icons/icon-192x192.png',
-  'https://raw.githubusercontent.com/pwa-builder/pwa-starter/main/public/assets/icons/icon-512x512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
